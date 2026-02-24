82ccd6f7fa098d8d310a94cd6a677ad81fc32fef
wagnerarins <137831201+wagnerarins@users.noreply.github.com>
2026-02-24 16:50:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,14 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 def get_sfs():
-    options = Options()
-    options.add_argument('--headless')
+options = Options()
+    options.add_argument('--headless=new') 
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--window-size=1920,1080')
+    # User-Agent
+    options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36')
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     try:
         driver.get("https://praticagemsf.com.br/movimentacoes/")
